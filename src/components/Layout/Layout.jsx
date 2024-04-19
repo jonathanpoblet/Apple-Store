d8f6b2fe10aeb0499cbb333a01d362f6c5f5c8d3
unknown <jonathanpoblet228@gmail.com>
2024-04-19 10:12:46 -0300

    Added Layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+import "./layout.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className='fade-in' id='layout'>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
