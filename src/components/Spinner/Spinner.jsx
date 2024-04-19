b5350949148d2ddbc1c7ad160f650ac2de6efc70
unknown <jonathanpoblet228@gmail.com>
2024-04-19 10:14:01 -0300

    Added Spinner

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import { ImSpinner9 } from "react-icons/im";
+import "./spinner.css";
+
+export default function Spinner() {
+  return (
+    <div className='spinner'>
+      <ImSpinner9 className='spinner-icon' />
+    </div>
+  );
+}
