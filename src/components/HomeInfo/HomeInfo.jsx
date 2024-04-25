ec3112262072951bf12961567221af017e345978
unknown <jonathanpoblet228@gmail.com>
2024-04-25 15:18:04 -0300

    HomeInfo Change Space Boxes

--- src/components/HomeInfo/HomeInfo.jsx
+++ src/components/HomeInfo/HomeInfo.jsx
@@ -17,15 +17,15 @@ export default function HomeInfo() {
         <h3 className='home-info-container-white-title'>Garantía</h3>
         <p className='home-info-container-white-text'>Garantía oficial de Apple por 1 año</p>
       </div>
-      <div className='home-info-container-white'>
-        <FaBox className='home-info-container-white-icon' />
-        <h3 className='home-info-container-white-title'>Devolución</h3>
-        <p className='home-info-container-white-text'>10 días posteriores a la compra</p>
-      </div>
       <div className='home-info-container-black'>
         <BsFillHandThumbsUpFill className='home-info-container-black-icon' />
         <h3 className='home-info-container-black-title'>Servicio</h3>
         <p className='home-info-container-black-text'>Asesoramiento personalizado 100%</p>
+      </div>
+      <div className='home-info-container-white'>
+        <FaBox className='home-info-container-white-icon' />
+        <h3 className='home-info-container-white-title'>Devolución</h3>
+        <p className='home-info-container-white-text'>10 días posteriores a la compra</p>
       </div>
     </section>
   );
